d1d66c97996707dc75306b375420cc4ae2fa7e2f
akbarsanihasan <akbarsani.dev@gmail.com>
2025-04-18 14:15:48 +0700

    update: git, lualine

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,24 +1,28 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "│" },
-				change = { text = "│" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
-			},
-		},
-	},
-	{
-		"tpope/vim-fugitive",
-		keys = {
-			{ "<leader>ga", ":Git<CR>", silent = true, noremap = true },
-			{ "<leader>gb", ":Git blame<CR>", silent = true, noremap = true },
-			{ "gf", "diffget //2<CR>", silent = true, noremap = true },
-			{ "gj", "diffget //3<CR>", silent = true, noremap = true },
-		},
-	},
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {},
+        keys = {
+            {
+                "<leader>gb",
+                ":Git blame_line<CR>",
+                silent = true,
+                noremap = true,
+            },
+            {
+                "<leader>gB",
+                ":Git blame<CR>",
+                silent = true,
+                noremap = true,
+            },
+        },
+    },
+    {
+        "tpope/vim-fugitive",
+        keys = {
+            { "<leader>ga", ":Git<CR>",        silent = true, noremap = true },
+            { "gf",         "diffget //2<CR>", silent = true, noremap = true },
+            { "gj",         "diffget //3<CR>", silent = true, noremap = true },
+        },
+    },
 }
